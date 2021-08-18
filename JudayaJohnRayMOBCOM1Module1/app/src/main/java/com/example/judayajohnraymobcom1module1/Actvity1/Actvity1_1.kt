5af9694a9a87bdcc36b237a637339e4c0a774314
Niblegen <judaya93@gmail.com>
2021-08-18 14:56:17 +0800

    Activity 1.1.1 Variables and Data Types

---
+++ JudayaJohnRayMOBCOM1Module1/app/src/main/java/com/example/judayajohnraymobcom1module1/Actvity1/Actvity1_1.kt
@@ -0,0 +1,19 @@
+package com.example.judayajohnraymobcom1module1.Actvity1
+
+fun main(){
+    val b = 69
+
+    val weight :Float = 4.20F
+
+    val weightUnit = "lbs"
+
+    val patient: String
+
+    patient = "Judaya, John Ray"
+
+    var age: Int
+
+    var nukeCode = 0x730d06
+
+    var aVAR: Short
+}
--- JudayaJohnRayMOBCOM1Module1/app/src/main/java/com/example/judayajohnraymobcom1module1/MainProgram.kt
+++ JudayaJohnRayMOBCOM1Module1/app/src/main/java/com/example/judayajohnraymobcom1module1/MainProgram.kt
@@ -1,19 +1,5 @@
 package com.example.judayajohnraymobcom1module1
 
 fun main(){
-    val b = 69
-
-    val value:Float = 18.5F
-
-    val randomVal = "lbs"
-
-    val patient: String
-
-    patient = "Zombie"
-
-    var age = 20
-
-    var myMutableVariable = 0x730d06
-
-//    val  = 1
+    print("Hello world")
 }
